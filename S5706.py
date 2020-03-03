797884c481275691a45a85df0cbbe4825df42446
Gyula Sallai <gyula.sallai@sonarsource.com>
2020-03-03 12:12:18 +0100

    S5706.py should only raise S5706 issues

--- S5706.py
+++ S5706.py
@@ -1,9 +1,11 @@
-class BareRaiseInExit:
-    def __exit__(self, exc_type, exc_value, traceback):
-        raise # Noncompliant {{Remove this "raise" statement and return "False" instead.}}
-       #^^^^^
+"""This module tests S5706"""
 
-class ReRaisingExceptionValue:
+class BareRaiseInExit(object):
+    """A case where a bare raise is present in __exit__"""
     def __exit__(self, exc_type, exc_value, traceback):
-        raise exc_value # Noncompliant
-       #^^^^^^^^^^^^^^^
+        raise
+
+class ReRaisingExceptionValue(object):
+    """A case where we re-raise the exception in __exit__"""
+    def __exit__(self, exc_type, exc_value, traceback):
+        raise exc_value
