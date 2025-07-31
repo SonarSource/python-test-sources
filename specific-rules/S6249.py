6be8e4cba69867229c71913c51d69004bea4f6ca
Sebastian Zumbrunn <sebastian.zumbrunn@sonarsource.com>
2025-07-31 17:00:16 +0200

    SONARPY-3220 add rule specific files

---
+++ specific-rules/S6249.py
@@ -0,0 +1,73 @@
+import aws_cdk.aws_iam as iam
+import aws_cdk.aws_s3 as s3
+
+no_config = s3.Bucket(self, "bucket")  # Noncompliant 
+
+ssl_false = s3.Bucket(self, "bucket", enforce_ssl=False)  # Noncompliant 
+
+
+with_config = s3.Bucket(self, "bucket")
+
+result = with_config.add_to_resource_policy(iam.PolicyStatement(  # Noncompliant 
+        effect=iam.Effect.DENY,
+        resources=[bucket.bucket_arn],
+        actions=["s3:SomeAction"],
+        principals=[roles],
+        conditions=[{"Bool": {"aws:SecureTransport": False}}],
+    )
+)
+
+empty_policy_call = s3.Bucket(self, "bucket") # Noncompliant
+result = empty_policy_call.add_to_resource_policy()
+
+no_policy_added = s3.Bucket(self, "bucket") # Noncompliant
+result = no_policy_added.foo( 
+    iam.PolicyStatement(  
+        effect=iam.Effect.DENY,
+        resources=["*"],
+        actions=["s3:*"],
+        principals=["*"],
+        conditions=["SecureTransport:False"],
+    )
+)
+
+not_policy_statement = s3.Bucket(self, "bucket") # Noncompliant
+result = not_policy_statement.add_to_resource_policy( 
+    iam.Foo(  
+        effect=iam.Effect.DENY,
+        resources=["*"],
+        actions=["s3:*"],
+        principals=["*"],
+        conditions=["SecureTransport:False"],
+    )
+)
+from module import foo
+
+ssl_true = s3.Bucket(self, "bucket", enforce_ssl=True)  # Compliant
+ssl_unknown = s3.Bucket(self, "bucket", enforce_ssl=foo())  # Compliant
+
+correct_policy = s3.Bucket(self, "bucket")
+result = correct_policy.add_to_resource_policy(
+    iam.PolicyStatement(  # Compliant
+        effect=iam.Effect.DENY,
+        resources=["*"],
+        actions=["s3:*"],
+        principals=["*"],
+        conditions=["SecureTransport:False"],
+    )
+)
+
+compliant_policy = s3.Bucket(self, "bucket")
+result = compliant_policy.add_to_resource_policy(
+    iam.PolicyStatement(  # Compliant
+        effect=iam.Effect.DENY,
+        resources=["foo", "*"],
+        actions=["s3:*", "foo:foo"],
+        principals=[
+            "role",
+            "other_role",
+            "*",
+        ],
+        conditions=["condition_a", "SecureTransport:False"],
+    )
+)
